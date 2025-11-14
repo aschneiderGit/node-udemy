37ba4d28d7db29d418836aa3e3eece7637938ae4
aschneiderGit <aimeschneider68@gmail.com>
2025-11-14 15:47:11 +0100

    3.35 buffer and req event

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 const http = require("http");
+const fs = require("fs");
 
 //function rqListener(req, res) {} anonymized in the createServer() param
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="m"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log("chunk");
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      console.log("body");
+      console.log(body);
+      const parsedBody = Buffer.concat(body).toString("utf-8");
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message); //block the code until the file is created
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head><title>My Node App</title></head>");
-  res.write("<body><h1>Hello world !</h1></body>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
   res.end();
 });
