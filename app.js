e391fdc668ef51b08ffe14480621b4f654228fb2
aschneiderGit <aimeschneider68@gmail.com>
2025-11-14 15:27:55 +0100

    3.31 res

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const http = require("http");
 //function rqListener(req, res) {} anonymized in the createServer() param
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Node App</title></head>");
+  res.write("<body><h1>Hello world !</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
