30259b2c80b0c4783382a0c1373e83b9b2fa7b15
aschneiderGit <aimeschneider68@gmail.com>
2025-11-20 17:26:20 +0100

    return html to request

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
+const path = require("path");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express</h1>");
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
