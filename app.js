b4246a89704c8a41130df6b29c91ee1d9cc38b9c
aschneiderGit <aimeschneider68@gmail.com>
2025-11-20 17:11:49 +0100

    route folder and file

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit"> Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.get("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
